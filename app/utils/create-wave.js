76248a6893b487001537126093ae9794ce2fb9f8
Channing Allen <channingallen@gmail.com>
2015-12-22 11:45:18 -0800

    Create first wave

---
+++ app/utils/create-wave.js
@@ -0,0 +1,43 @@
+import Board from '../objects/board';
+import Mob from '../objects/mob';
+import TowerGroup from '../objects/tower-group';
+import Tower from '../objects/tower';
+import Wave from '../objects/wave';
+
+function addBoardToWave(wave) {
+  const board = Board.create();
+  // TODO THIS COMMIT: give board `imageUrl`
+  // TODO THIS COMMIT: give board `pathData`
+  wave.set('board', board);
+}
+
+function addMobsToWave(wave) {
+  const mob = Mob.create();
+  mob.set('maxHealth', 100); // TODO THIS COMMIT: adjust this
+  // TODO THIS COMMIT: set mob `path`
+  mob.set('points', 1); // TODO THIS COMMIT: adjust this
+  mob.set('quantity', 10); // TODO THIS COMMIT: adjust this
+  mob.set('speed', 1); // TODO THIS COMMIT: adjust this
+  mob.set('type', 'standard'); // TODO THIS COMMIT: adjust this
+
+  wave.set('mobs', [mob]);
+}
+
+function addTowerGroupsToWave(wave) {
+  const towerGroup = TowerGroup.create();
+  const tower = Tower.create();
+  tower.set('type', 1); // TODO THIS COMMIT: adjust this
+  towerGroup.set('tower', tower);
+
+  wave.set('towerGroups', [towerGroup]);
+}
+
+export default function createWave() {
+  const wave = Wave.create({ minimumScore: 3 });
+
+  addBoardToWave(wave);
+  addMobsToWave(wave);
+  addTowerGroupsToWave(wave);
+
+  return wave;
+}
---
+++ tests/unit/utils/create-wave-test.js
@@ -0,0 +1,10 @@
+import createWave from '../../../utils/create-wave';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | create wave');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = createWave();
+  assert.ok(result);
+});
