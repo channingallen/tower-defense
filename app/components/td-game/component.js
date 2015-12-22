be262fe02aec8058c63a714b6e9014c9e3dbccd0
Channing Allen <channingallen@gmail.com>
2015-12-22 12:15:24 -0800

    Implement start button

--- app/components/td-game/component.js
+++ app/components/td-game/component.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import createWave from 'tower-defense/utils/create-wave';
 
 export default Ember.Component.extend({
+  classNames: ['td-game'],
+
+  currentWave: createWave(),
+
+  waveStarted: false,
+
+  actions: {
+    startWave() {
+      this.set('waveStarted', true);
+    }
+  }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,4 @@
-/*jshint node:true*/
+/* jshint node:true */
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
