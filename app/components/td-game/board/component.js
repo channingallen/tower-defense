90949c45948667504a9e461bbea40b2a137c0bdd
Channing Allen <channingallen@gmail.com>
2016-01-12 13:53:01 -0800

    Fire on mobs in range of towers

--- app/components/td-game/board/component.js
+++ app/components/td-game/board/component.js
@@ -50,6 +50,51 @@ export default Ember.Component.extend({
     });
   },
 
+  _reduceMobHealth(mobId, healthToReduce) {
+    if (!healthToReduce) {
+      healthToReduce = 20;
+    }
+
+    this.get('mobs').forEach((mob) => {
+      if (mobId === mob.get('id')) {
+        const currentHealth = mob.get('remainingHealth');
+        mob.set('remainingHealth', currentHealth - healthToReduce);
+      }
+    });
+  },
+
+  _mobInRangeOfTower(mob, tower, range) {
+    function getDistance(mob, tower) {
+      var latDiff = Math.abs(tower.get('posX') - mob.get('posX'));
+      var lngDiff = Math.abs(tower.get('posY') - mob.get('posY'));
+      return latDiff + lngDiff;
+    }
+
+    return getDistance(mob, tower) < range ? true : false;
+  },
+
+  attackMobsInTowerRange: Ember.observer('attrs.waveStarted', function () {
+    setInterval(() => {
+      if (!this.get('allTowers.length') || !this.get('mobs.length')) {
+        return;
+      }
+
+      this.get('allTowers').forEach((tower) => {
+        this.get('mobs').forEach((mob) => {
+          if (this._mobInRangeOfTower(mob, tower, 100)) { // TODO: replace arg 3 with tower property for attack range, based on type?
+            this._reduceMobHealth(mob.get('id'), 20); // TODO: replace arg 2 with tower property for attack power, based on type?
+          }
+        });
+      });
+
+      // this.get('mobs').forEach((mob) => {
+      //   if (this._towersInRange(mob)) {
+      //     this._reduceMobHealth(mob.get('id'), 20);
+      //   }
+      // });
+    }, 500);
+  }),
+
   getTowers: Ember.observer('attrs.waveStarted', function () {
     this.attrs.towerGroups.forEach((towerGroup) => {
       this.get('allTowers').pushObject(towerGroup);
