7904e073e49ce7afa0d0b5577be32ffde8dfcd93
Channing Allen <channingallen@gmail.com>
2016-01-16 13:12:14 -0800

    Implement shake action in stylesheet

--- app/components/td-game/stylesheet/component.js
+++ app/components/td-game/stylesheet/component.js
@@ -7,7 +7,33 @@ export default Ember.Component.extend({
 
   twrGrpStyles: null,
 
+  shakeActive: false,
+
+  _delayNextShake() {
+    this.set('shakeActive', true);
+
+    Ember.run.later(this, () => {
+      this.set('shakeActive', false);
+    }, 1300);
+  },
+
   actions: {
+    shake() {
+      if (this.get('shakeActive')) {
+        return;
+      }
+
+      this._delayNextShake();
+
+      let l = 20;
+      for (let i = 0; i < 10; i++) {
+        this.$().animate({
+          'margin-left': "+=" + (l = -l) + 'px',
+          'margin-right': "-=" + l + 'px'
+        }, 115);
+      }
+    },
+
     startWave() {
       this.attrs['start-wave']();
     },
