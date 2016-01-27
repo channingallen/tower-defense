10a00b6ada931ecc85b0b40c4c4cb7d3b461beec
Channing Allen <channingallen@gmail.com>
2016-01-27 14:14:22 -0800

    Improve projectile homing logic

--- app/components/td-game/board/projectile/component.js
+++ app/components/td-game/board/projectile/component.js
@@ -5,11 +5,29 @@ export default Ember.Component.extend({
 
   inFlight: false,
 
-  _closeInOnTarget() {
-    if (!this._targetReached()) {
-      this._setPosition(this.attrs.mobX, this.attrs.mobY); // takes 200 milliseconds
+  targetLeftA: null,
 
-      this._fetchNewTargetCoords();
+  targetTopA: null,
+
+  _closeInOnTarget2() {
+    const targetLeftB = this._getPercentageLeft(this._getTargetLeftPosition());
+    const targetTopB = this._getPercentageTop(this._getTargetTopPosition());
+    const targetFound = !!targetLeftB && !!targetTopB;
+
+    if (targetFound && !this._targetReached(targetLeftB, targetTopB)) {
+      const targetLeftA = this.get('targetLeftA');
+      const targetTopA = this.get('targetTopA');
+
+      const targetLeftC = this._getTargetLeftC(targetLeftA, targetLeftB);
+      const targetTopC = this._getTargetTopC(targetTopA, targetTopB);
+
+      this._setPosition(targetLeftC, targetTopC);
+
+      this._setTargetPosA();
+
+      Ember.run.later(this, () => {
+        this._closeInOnTarget2();
+      }, 21);
     } else {
       this.set('inFlight', false);
 
@@ -18,6 +36,8 @@ export default Ember.Component.extend({
   },
 
   _destroy() {
+    this.set('newTargetCoordsEventCount', 0);
+
     this.set('inFlight', false);
 
     this.attrs.destroy(this.attrs.id);
@@ -27,48 +47,100 @@ export default Ember.Component.extend({
     this.attrs['update-target-coords'](this.attrs.id, this.attrs.mobId);
   },
 
+  _getDiff(numA, numB) {
+    return Math.abs(numA - numB);
+  },
+
   _getNumFromPx(pixels) {
     const valWithoutPct = pixels.split('px')[0];
     return parseInt(valWithoutPct, 10);
   },
 
   _getPercentageLeft(crudeLeft) {
+    if (!crudeLeft) {
+      return undefined;
+    }
+
     const left = this._getNumFromPx(crudeLeft);
-    const boardWidth = this.$().parent().width();
+    const boardWidth = Ember.$('.td-game__board').width();
+
     return (left / boardWidth) * 100;
   },
 
   _getPercentageTop(crudeTop) {
+    if (!crudeTop) {
+      return undefined;
+    }
+
     const top = this._getNumFromPx(crudeTop);
-    const boardHeight = this.$().parent().height();
+    const boardHeight = Ember.$('.td-game__board').height();
+
     return (top / boardHeight) * 100;
   },
 
+  _getTargetLeftC(targetLeftA, targetLeftB) {
+    if (targetLeftA > targetLeftB) {
+      return targetLeftB - (this._getDiff(targetLeftA, targetLeftB) * 10);
+    } else {
+      return targetLeftB + (this._getDiff(targetLeftA, targetLeftB) * 10);
+    }
+  },
+
+  _getTargetLeftPosition() {
+    return Ember.$(`#${this.attrs.mobId}`).css('left');
+  },
+
+  _getTargetTopC(targetTopA, targetTopB) {
+    if (targetTopA > targetTopB) {
+      return targetTopB - (this._getDiff(targetTopA, targetTopB) * 10);
+    } else {
+      return targetTopB + (this._getDiff(targetTopA, targetTopB) * 10);
+    }
+  },
+
+  _getTargetTopPosition() {
+    return Ember.$(`#${this.attrs.mobId}`).css('top');
+  },
+
   _setPosition(left, top) {
-    this.$().css('position', 'absolute');
     this.$().css('left', left + '%');
     this.$().css('top', top + '%');
   },
 
-  _targetReached() {
-    const crudeLeft = this.$().css('left');
-    const crudeTop = this.$().css('top');
+  _setTargetPosA() {
+    const crudeTargetLeftA = this._getTargetLeftPosition();
+    const crudeTargetTopA = this._getTargetTopPosition();
 
-    const leftPctOfTotalWidth = this._getPercentageLeft(crudeLeft);
-    const topPctOfTotalHeight = this._getPercentageTop(crudeTop);
+    this.set('targetLeftA', this._getPercentageLeft(crudeTargetLeftA));
+    this.set('targetTopA', this._getPercentageTop(crudeTargetTopA));
+  },
 
-    var latDiff = Math.abs(leftPctOfTotalWidth - this.attrs.mobX);
-    var lngDiff = Math.abs(topPctOfTotalHeight - this.attrs.mobY);
+  _targetReached(targetLeft, targetTop) {
+    const crudeProjectileLeft = this.$().css('left');
+    const crudeProjectileTop = this.$().css('top');
+
+    const projectileLeft = this._getPercentageLeft(crudeProjectileLeft);
+    const projectileTop = this._getPercentageTop(crudeProjectileTop);
+
+    var latDiff = Math.abs(projectileLeft - targetLeft);
+    var lngDiff = Math.abs(projectileTop - targetTop);
 
     const totalDiff = latDiff + lngDiff;
 
-    return totalDiff < 1;
+    return totalDiff < 2;
   },
 
   _placeProjectile: Ember.on('didInsertElement', function () {
+    console.log(`PROJECTILE ID: ${this.attrs.id}`); // TODO THIS COMMIT: remove this
     this._setPosition(this.attrs.towerX + 1, this.attrs.towerY + 1);
 
     this.set('inFlight', true);
+
+    this._setTargetPosA();
+
+    Ember.run.later(this, () => {
+      this._closeInOnTarget2();
+    }, 20);
   }),
 
   _selfDestructAfterTimeout: Ember.on('didInsertElement', function () {
@@ -78,19 +150,20 @@ export default Ember.Component.extend({
 
         this._destroy();
       }
-    }, 350); // determined with respect to 0.3s CSS transition
+    }, 400); // determined with respect to 0.3s CSS transition
   }),
 
-  _updateTargetCoords: Ember.observer(
-    'inFlight',
-    'attrs.mobX',
-    'attrs.mobY',
-    function () {
-      if (this.get('inFlight')) {
-        Ember.run.later(this, () => {
-          this._closeInOnTarget();
-        }, 20);
-      }
-    }
-  )
+  // _updateTargetCoords: Ember.observer(
+  //   'inFlight',
+  //   'attrs.mobX',
+  //   'attrs.mobY',
+  //   function () {
+  //     if (this.get('inFlight')) {
+  //
+  //       Ember.run.later(this, () => {
+  //         this._closeInOnTarget();
+  //       }, 10);
+  //     }
+  //   }
+  // )
 });
