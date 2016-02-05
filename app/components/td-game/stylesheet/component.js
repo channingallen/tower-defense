4785885b17e4b4bf97d68a631d726b8177e42378
Channing Allen <channingallen@gmail.com>
2016-02-05 14:23:51 -0800

    Fix stylesheet reset bug

--- app/components/td-game/stylesheet/component.js
+++ app/components/td-game/stylesheet/component.js
@@ -17,26 +17,22 @@ export default Ember.Component.extend({
     }, 1300);
   },
 
-  _resetStylesheet: Ember.observer('attrs.waveStarted', function () {
-    if (!this.attrs.waveStarted) {
-      this.set('finalTowerId', null);
-      this.set('firstTowerGroupId', null);
-      this.set('shakeActive', false);
-    }
-  }),
+  _setFirstAndFinalUnitIds: Ember.on('init', Ember.observer(
+    'attrs.currentWaveNumber',
+    'attrs.waveStarted',
+    function () {
+      if (!this.attrs.waveStarted) {
+        const firstTowerGroup = this.attrs.towerGroups.get('firstObject');
+        const firstTowerGroupId = firstTowerGroup.get('id');
+        this.set('firstTowerGroupId', firstTowerGroupId);
 
-  _setFirstAndFinalUnitIds: Ember.on('init', Ember.observer('attrs.waveStarted', function () {
-    if (!this.attrs.waveStarted) {
-      const firstTowerGroup = this.attrs.towerGroups.get('firstObject');
-      const firstTowerGroupId = firstTowerGroup.get('id');
-      this.set('firstTowerGroupId', firstTowerGroupId);
-
-      const finalTowerGroup = this.attrs.towerGroups.get('lastObject');
-      const finalTower = finalTowerGroup.get('towers').get('lastObject');
-      const finalTowerId = finalTower.get('id');
-      this.set('finalTowerId', finalTowerId);
+        const finalTowerGroup = this.attrs.towerGroups.get('lastObject');
+        const finalTower = finalTowerGroup.get('towers').get('lastObject');
+        const finalTowerId = finalTower.get('id');
+        this.set('finalTowerId', finalTowerId);
+      }
     }
-  })),
+  )),
 
   actions: {
     shake() {
