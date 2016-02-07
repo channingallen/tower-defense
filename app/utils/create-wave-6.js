51a09ac91513f1545959befe1f5b1bb48be1d4f5
Channing Allen <channingallen@gmail.com>
2016-02-07 10:02:15 -0800

    Import and implement sixth wave image

--- app/utils/create-wave-6.js
+++ app/utils/create-wave-6.js
@@ -9,7 +9,7 @@ import Wave from 'tower-defense/objects/wave';
 
 function addBoardToWave(wave) {
   const board = Board.create();
-  board.set('imageUrl', '/images/path-0.png');
+  board.set('imageUrl', '/images/path-6.png');
 
   const pathObjects = [
     PathCoords.create({ x: -5, y: 35 }),
