fcade8036579b2cdbf6b0150c28b8f3b23e4412a
Channing Allen <channingallen@gmail.com>
2016-02-08 13:23:35 -0800

    Create wave 7 file

---
+++ app/utils/create-wave-7.js
@@ -0,0 +1,108 @@
+import Board from 'tower-defense/objects/board';
+import createUnitCodeLine from 'tower-defense/utils/create-unit-code-line';
+import Ember from 'ember';
+import Mob from 'tower-defense/objects/mob';
+import PathCoords from 'tower-defense/objects/path-coords';
+import TowerGroup from 'tower-defense/objects/tower-group';
+import Tower from 'tower-defense/objects/tower';
+import Wave from 'tower-defense/objects/wave';
+
+function addBoardToWave(wave) {
+  const board = Board.create();
+  board.set('imageUrl', '/images/path-0.png');
+
+  const pathObjects = [
+    PathCoords.create({ x: 65, y: 105 })
+  ];
+
+  pathObjects.forEach((pathObject) => {
+    board.get('pathData').pushObject(pathObject);
+  });
+
+  wave.set('board', board);
+}
+
+function addMobsToWave(wave) {
+  const mobs = [];
+
+  const mobQuantity = 5;
+  for (var i = 0; i < mobQuantity; i++) {
+    const newMob = Mob.create({
+      id: generateIdForRecord(),
+      frequency: 1500,
+      health: 300,
+      maxHealth: 300,
+      points: 20,
+      quantity: mobQuantity,
+      speed: 10, // seconds to cross one axis of the board
+      type: 'standard'
+    });
+
+    mobs.push(newMob);
+  }
+  wave.set('mobs', Ember.A(mobs));
+}
+
+function addTowerGroupsToWave(wave) {
+  let groupNum = 1;
+
+  function getNewTowerGroup(numRows, posY) {
+    return TowerGroup.create({
+      id: generateIdForRecord(),
+      numRows: numRows,
+      posY: 'board__tower-group--position-y' + posY,
+      selector: '.tg' + groupNum++,
+      styles: Ember.A([createUnitCodeLine()])
+    });
+  }
+
+  const towerGroup1 = getNewTowerGroup(3, 35);
+
+  addTowersToTowerGroup(towerGroup1, [
+    { type: 1 }, { type: 2 }, { type: 1 }, { type: 2 }
+  ]);
+
+  wave.set('towerGroups', Ember.A([towerGroup1]));
+}
+
+function addTowersToTowerGroup(towerGroup, specsForTowers) {
+  function getNewTower(towerNum, type) {
+    return Tower.create({
+      id: generateIdForRecord(),
+      attackPower: 20,
+      attackRange: 20,
+      selector: 't' + towerNum,
+      type,
+      styles: Ember.A([createUnitCodeLine()])
+    });
+  }
+
+  let newTowers = [];
+  for (var i = 1; i < specsForTowers.length + 1; i++) {
+    newTowers.pushObject(getNewTower(i, specsForTowers.objectAt(i - 1).type));
+  }
+
+  towerGroup.set('towers', newTowers);
+}
+
+function generateIdForRecord() {
+  function generate4DigitString() {
+    const baseInt = Math.floor((1 + Math.random()) * 0x10000);
+    return baseInt.toString(16).substring(1);
+  }
+
+  return generate4DigitString() + generate4DigitString() + '-' +
+         generate4DigitString() + '-' + generate4DigitString() + '-' +
+         generate4DigitString() + '-' + generate4DigitString() +
+         generate4DigitString() + generate4DigitString();
+}
+
+export default function createWave7() {
+  const wave = Wave.create({ minimumScore: 40 });
+
+  addBoardToWave(wave);
+  addMobsToWave(wave);
+  addTowerGroupsToWave(wave);
+
+  return wave;
+}
