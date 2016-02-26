265fb04abe5228b99d242cd0ba35d4b25fb13737
Channing Allen <channingallen@gmail.com>
2016-02-26 15:11:29 -0800

    Keep right-aligned tower tooltips on the screen

--- app/components/tool-tip.js
+++ app/components/tool-tip.js
@@ -7,20 +7,31 @@ import Ember from 'ember';
 ////////////////
 
 const ToolTipComponent = Ember.Component.extend({
-  classNameBindings: ['atTop:tool-tip--bottom:tool-tip--top'],
+  classNameBindings: ['atTop:tool-tip--below:tool-tip--above'],
 
   classNames: ['tool-tip'],
 
   atTop: null,
 
-  checkOffsetTop: Ember.on('didInsertElement', function () {
-    if (!this || !this.$()) {
-      return;
-    }
-
+  updateAtTop: Ember.on('didInsertElement', function () {
     const atTop = this.$().offset().top < 100;
     this.set('atTop', atTop);
-  })
+  }),
+
+  keepTowerToolTipsOnScreen: Ember.on('didInsertElement', Ember.observer('atTop', function () {
+    const windowWidth = Ember.$(window).width();
+    const offsetLeft = this.$().offset  ().left;
+    const textOuterWidth = this.$('nobr').outerWidth();
+    const offsetLeftPlusOuterWidth = offsetLeft + textOuterWidth;
+    const offsetRight = windowWidth - offsetLeftPlusOuterWidth;
+    const atRight = offsetRight < 0;
+
+    if (this.attrs.type === 'tower' && atRight) {
+      Ember.run.schedule('afterRender', this, () => {
+        this.$('.tool-tip__content').css('margin-left', `${offsetRight}px`);
+      });
+    }
+  }))
 });
 
 export default ToolTipComponent;
