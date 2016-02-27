67992b6c6d5b8c11a2f01cc4f41aa03a58a88c10
Channing Allen <channingallen@gmail.com>
2016-02-27 14:11:07 -0800

    Import graphic for wave 4

--- app/utils/create-wave-4.js
+++ app/utils/create-wave-4.js
@@ -9,7 +9,7 @@ import Wave from 'tower-defense/objects/wave';
 
 function addBoardToWave(wave) {
   const board = Board.create();
-  board.set('imageUrl', '/images/path-0.png');
+  board.set('imageUrl', '/images/path-4.png');
 
   const pathObjects = [
     PathCoords.create({ x: 0, y: 30 }),
