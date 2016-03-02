af167064d5270b95f7654ceb72b849ac00492868
Channing Allen <channingallen@gmail.com>
2016-03-02 11:12:30 -0800

    Provide an example for the first wave

--- app/utils/create-wave-1.js
+++ app/utils/create-wave-1.js
@@ -119,7 +119,10 @@ items horizontally and accepts the following values:
 * \`space-between\`: evenly distribute items in a container such that the first
 item aligns to the left and the final item aligns to the right
 * \`space-around\`: evenly distribute items in a container such that all items
-have equal space around them`,
+have equal space around them
+
+To move the container's towers to the center, for example, try
+\`justify-content: center;\``,
       tldr: `Use the \`justify-content\` property to move these two towers into
            position. Click the <i class="fa fa-question-circle"></i> button in
            the stylesheet for a reminder on how the property works.`
